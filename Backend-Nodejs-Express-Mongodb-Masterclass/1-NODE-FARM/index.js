0fcea961da0f917e6f8729ed276cb485ce29ea3e
Zoe Oladokun <145172902+zozoking101@users.noreply.github.com>
2025-02-10 19:24:50 +0100

    Mon codes commit

--- Backend-Nodejs-Express-Mongodb-Masterclass/1-NODE-FARM/index.js
+++ Backend-Nodejs-Express-Mongodb-Masterclass/1-NODE-FARM/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify'); // ^ wants all, ~ needs the least updated, npm outdated
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -51,6 +52,10 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8', );
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true}))
+console.log(slugs)
+// console.log(slugify('Fresh Avocados', { lower: true}))
 
 const server = http.createServer((req, res) => {
     
